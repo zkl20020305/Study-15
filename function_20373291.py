d19fa8918be00ad5b2414b8ff86c8c2f71a91366
zkl20020305 <73997958+zkl20020305@users.noreply.github.com>
2022-03-20 20:32:32 +0800

    Delete function_20373291.py

--- function_20373291.py
+++
@@ -1,3 +0,0 @@
-Python 3.9.9 (tags/v3.9.9:ccb0e6a, Nov 15 2021, 18:08:50) [MSC v.1929 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> print('My student_id: 20373291')
